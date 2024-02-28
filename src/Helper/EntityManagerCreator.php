d2a1d1df3f0232c04c724df802bfecd17d5eafaf
Marcos Motta <mrcsmotta1@gmail.com>
2024-02-28 17:10:57 -0300

    first commit

---
+++ src/Helper/EntityManagerCreator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Alura\Doctrine\Helper;
+
+use Doctrine\ORM\ORMSetup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\DriverManager;
+
+class EntityManagerCreator
+{
+    public static function createEntityManager(): EntityManager
+    {
+        // Create a simple "default" Doctrine ORM configuration for Attributes
+        $config = ORMSetup::createAttributeMetadataConfiguration(
+            [__DIR__ . "/src"],
+            true,
+        );
+
+        // configuring the database connection
+        $connection = DriverManager::getConnection([
+            'driver' => 'pdo_sqlite',
+            'path' => __DIR__ . '/db.sqlite',
+        ], $config);
+
+        // obtaining the entity manager
+        return new EntityManager($connection, $config);
+    }
+}
