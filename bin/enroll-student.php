312bd5a57cdf706f8a96118c660e6cfb68c3c43c
Marcos Motta <mrcsmotta1@gmail.com>
2024-03-01 18:57:04 -0300

    Implementado manyToMany, inserido curso e associado a Student

---
+++ bin/enroll-student.php
@@ -0,0 +1,18 @@
+<?php
+
+use Alura\Doctrine\Entity\Course;
+use Alura\Doctrine\Entity\Student;
+use Alura\Doctrine\Helper\EntityManagerCreator;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManager = EntityManagerCreator::createEntityManager();
+
+$studentID = $argv[1];
+$courseID = $argv[2];
+
+$student = $entityManager->find(Student::class, $studentID);
+$course = $entityManager->find(Course::class, $courseID);
+
+$student->enrollInCourse($course);
+$entityManager->flush();
