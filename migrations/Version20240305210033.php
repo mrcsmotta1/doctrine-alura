1ed5573d89e03d6bdaddd063f5a1d710168c9fc1
Marcos Motta <mrcsmotta1@gmail.com>
2024-03-05 18:29:04 -0300

    Instalando componente e criando Migrations

---
+++ migrations/Version20240305210033.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alura\Doctrine\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240305210033 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Criação de uma tabela de testes';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $table = $schema->createTable('teste');
+        $table->addColumn('id', 'integer')->setAutoincrement(true);
+        $table->addColumn('coluna_teste', 'string');
+
+        $table->setPrimaryKey(['id']);
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $schema->dropTable('teste');
+    }
+}
